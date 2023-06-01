63f63fb7a4888207ca0fbddbeca4693ab280dd47
someid <hannahpietersen@gmail.com>
2023-06-01 17:30:46 +0530

    added how to write classes

---
+++ practice.py
@@ -0,0 +1,17 @@
+'''class Myclass:
+    x = 5
+
+
+p1 = Myclass()
+print(p1.x) '''
+
+
+'''class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+p1 = Person('Hannah', 24)
+print(p1.name)
+print(p1.age) '''
